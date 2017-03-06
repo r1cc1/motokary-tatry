c59f2b06c5d03f9fc9dfc07c8a11b24491fdba1c
r1cc1 <gresrichie@live.com>
2017-03-06 22:45:22 +0100

    First Commit

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -11,4 +11,84 @@ angular.module('myApp.view1', ['ngRoute'])
 
 .controller('View1Ctrl', [function() {
 
+    function loadJSON(callback) {
+
+        var xobj = new XMLHttpRequest();
+        xobj.overrideMimeType("application/json");
+        xobj.open('GET', './view1/top-12-year.json', true); // Replace 'my_data' with the path to your file
+        xobj.onreadystatechange = function () {
+            if (xobj.readyState == 4 && xobj.status == "200") {
+                // Required use of an anonymous callback as .open will NOT return a value but simply returns undefined in asynchronous mode
+                callback(xobj.responseText);
+            }
+        };
+        xobj.send(null);
+    }
+
+    function loadPrices(callback) {
+
+        var xobj = new XMLHttpRequest();
+        xobj.overrideMimeType("application/json");
+        xobj.open('GET', './view1/prices.json', true); // Replace 'my_data' with the path to your file
+        xobj.onreadystatechange = function () {
+            if (xobj.readyState == 4 && xobj.status == "200") {
+                // Required use of an anonymous callback as .open will NOT return a value but simply returns undefined in asynchronous mode
+                callback(xobj.responseText);
+            }
+        };
+        xobj.send(null);
+    }
+
+    function init() {
+
+        loadJSON(function(response) {
+            // Parse JSON string into object
+            var actual_JSON = JSON.parse(response);
+
+            $.each(actual_JSON, function (index, value) {
+                var driverId = '<td>' + value.id + '</td>';
+                var driverName = '<td>' + value.name + '</td>';
+                var driverTime = '<td>' + value.time + '</td>';
+
+                $('#top-12').append( '<tr>'+
+                        driverId + driverTime + driverName
+                  + '</tr>');
+
+                $('#top-12-year').append( '<tr>'+
+                    driverId + driverTime + driverName
+                    + '</tr>');
+            });
+
+        });
+
+        loadPrices(function(response) {
+            // Parse JSON string into object
+            var actual_JSON = JSON.parse(response);
+
+            $.each(actual_JSON, function (index, value) {
+                var priceId = '<td>' + value.id + '</td>';
+                var priceTime = '<td>' + value.time + '</td>';
+                var price6hp = '<td>' + value.small + '</td>';
+                var price9hp = '<td>' + value.big + '</td>';
+
+                $('#prices-table').append( '<tr>'+
+                     priceTime + price6hp + price9hp
+                    + '</tr>');
+
+            });
+
+        });
+
+    }
+
+
+
+    $(document).ready(function () {
+
+      init();
+
+
+
+    });
+
 }]);
