c9098643329ad5c9950a2fcc6be5a0ade8a0d299
r1cc1 <gresrichie@live.com>
2017-04-11 16:30:32 +0200

    Final Update before release

--- app/eng/eng.js
+++ app/eng/eng.js
@@ -12,21 +12,88 @@ angular.module('myApp.eng', ['ngRoute'])
     console.log('init engCtrl');
     $scope.lang = 'eng';
 
-    console.log($scope.lang);
+    $scope.mainTitle = 'Ready to Win?';
 
-    $scope.category_1 = 'Records';
-    $scope.category_2 = 'News';
+    $scope.category_1 = 'News';
+    $scope.category_2 = 'Records';
     $scope.category_3 = 'Video';
     $scope.category_4 = 'Open Hours';
     $scope.category_5 = 'Info';
-    $scope.category_6 = 'Prices';
-    $scope.category_7 = 'Map';
+    $scope.category_6 = 'Photos';
+    $scope.category_7 = 'Prices';
+    $scope.category_8 = 'Map';
+
+    $scope.newsText = 'Reservations can be accepted ONLY BY PHONE CALL on number +421 949 388 748 after 2 p.m every day.  If you want to use discount coupon, you have to print it before arrival. We do not accept digital version. We are looking forward to see you soon, Motokary Tatry Team';
+
+    $scope.driverTime = 'Time';
+    $scope.driverName = 'Name';
+
+    $scope.motoTitle = 'Track and go karts';
+    $scope.motoText1 = 'Our go karts are equipped with Honda GX 200 (6,5 HP) and Honda GX 270 (9 HP). In order to drive 9HP go kart, you must finish at the certain time limit on 6,5 HP go kart.';
+    $scope.motoText2 = 'Track distance is 230 meters where track width is from 3.5 till 5.5 meters. It is possible to achieve 30 km/h average speed. Constant change of technical and fast track parts will definitely fulfill your expectations.';
+
+
+    $scope.oneKart = '1 Kart';
+    $scope.threeKart = '3 Karts';
+    $scope.fourKart = '4 Karts';
+    $scope.fiveKart = '5 Karts';
+
+    $scope.maskSell = 'Hygienic mask for sell 2,00 €';
+    $scope.maskRent = 'Hygienic mask for rent 1,00 €';
+
+    $scope.highlightText2 = 'You can enjoy our Bar and we organise team-buildings for companies on demand';
+
+    $scope.mapText = 'Go Kart Tatry is located on the second floor of the former production hall in Svit (see map), driving go karts will provide you high quality entertainment.';
+
+    $scope.day1 = 'Monday';
+    $scope.day2 = 'Tuesday';
+    $scope.day3 = 'Wednesday';
+    $scope.day4 = 'Thursday';
+    $scope.day5 = 'Friday';
+    $scope.day6 = 'Saturday';
+    $scope.day7 = 'Sunday';
+
+    $scope.warningTitle = 'Operating Instructions:';
+
+    $scope.rulesText0 = 'All the visitors must be informed with the following rules. If a visitor takes a ride, he/she automatically accepts and follows the rules.';
+    $scope.rulesText1 = 'Getting on/off a go-cart is permitted only at the depot advised by the staff';
+    $scope.rulesText2 = 'Driving in/out of the depot is permissible only with the approval of the staff';
+    $scope.rulesText3 = 'Drive through the depot slowly';
+    $scope.rulesText4 = 'In a case of a collision, getting out the go-cart is strictly prohibited. You can raise your hand, and the staff will help you out the collision';
+    $scope.rulesText5 = 'Any kind of contact with the other go-carts is prohibited';
+    $scope.rulesText6 = 'While driving, hold the steering wheel with both hands';
+    $scope.rulesText7 = 'Do not step or walk on the track';
+
+    $scope.rulesText8 = 'Respect all safety instructions from staff';
+    $scope.rulesText9 = 'Visitors under 15 must be accompanied by his/her parent';
+    $scope.rulesText10 = 'While driving, every visitor must wear mask and helmet';
+    $scope.rulesText11 = 'Any shawls and large clothing cannot be worn during the ride. Take any valuable items  out of your pockets';
+    $scope.rulesText12 = 'Be careful, at the back of the go-cart seat is hot exhaust stroke. Damage to you or your personnel things is your responsibility';
+    $scope.rulesText13 = 'Drive responsibly and fit your ride to your abilities';
+    $scope.rulesText14 = 'Any damage to your health or property is your own responsibility';
+    $scope.rulesText15 = 'By purchasing the ride you agree on our rules';
+    $scope.rulesText16 = 'If you do not follow our instructions, we may finish your ride earlier without any pay back';
+
 
     function loadJSON(callback) {
 
         var xobj = new XMLHttpRequest();
         xobj.overrideMimeType("application/json");
-        xobj.open('GET', './svk/top-12-year.json', true); // Replace 'my_data' with the path to your file
+        xobj.open('GET', './svk/top-year.json', true); // Replace 'my_data' with the path to your file
+        xobj.onreadystatechange = function () {
+            if (xobj.readyState == 4 && xobj.status == "200") {
+                // Required use of an anonymous callback as .open will NOT return a value but simply returns undefined in asynchronous mode
+                callback(xobj.responseText);
+            }
+        };
+        xobj.send(null);
+    }
+
+    function loadTopMonth(callback) {
+
+        var xobj = new XMLHttpRequest();
+        xobj.overrideMimeType("application/json");
+        xobj.open('GET', './svk/top-month.json', true); // Replace 'my_data' with the path to your file
         xobj.onreadystatechange = function () {
             if (xobj.readyState == 4 && xobj.status == "200") {
                 // Required use of an anonymous callback as .open will NOT return a value but simply returns undefined in asynchronous mode
@@ -83,16 +150,30 @@ angular.module('myApp.eng', ['ngRoute'])
                 var driverName = '<td>' + value.name + '</td>';
                 var driverTime = '<td>' + value.time + '</td>';
 
-                $('#top-12').append( '<tr>'+
-                    driverId + driverTime + driverName
-                    + '</tr>');
-
                 $('#top-12-year').append( '<tr>'+
                     driverId + driverTime + driverName
                     + '</tr>');
             });
-            $('#top-12').find('tr').first().addClass('first-place');
+
             $('#top-12-year').find('tr').first().addClass('first-place');
+        });
+
+
+        loadTopMonth(function(response) {
+            // Parse JSON string into object
+            var actual_JSON = JSON.parse(response);
+
+            $.each(actual_JSON, function (index, value) {
+                var driverId = '<td>' + value.id + '</td>';
+                var driverName = '<td>' + value.name + '</td>';
+                var driverTime = '<td>' + value.time + '</td>';
+
+                $('#top-12').append( '<tr>'+
+                    driverId + driverTime + driverName
+                    + '</tr>');
+            });
+
+            $('#top-12').find('tr').first().addClass('first-place');
         });
 
         loadPrices(function(response) {
--- app/svk/svk.js
+++ app/svk/svk.js
@@ -15,19 +15,87 @@ angular.module('myApp.svk', ['ngRoute'])
     // set Language
     $scope.lang = 'svk';
 
-    $scope.category_1 = 'Rekordy';
-    $scope.category_2 = 'Aktuality';
+    $scope.mainTitle = 'Ready to Win?';
+
+    $scope.category_1 = 'Aktuality';
+    $scope.category_2 = 'Rekordy';
     $scope.category_3 = 'Video';
-    $scope.category_4 = 'Otvaracie Hodiny';
+    $scope.category_4 = 'Otváracie Hodiny';
     $scope.category_5 = 'Info';
-    $scope.category_6 = 'Cenník';
-    $scope.category_7 = 'Mapa';
+    $scope.category_6 = 'Fotky';
+    $scope.category_7 = 'Cenník';
+    $scope.category_8 = 'Mapa';
+
+    $scope.newsText = 'Rezervácie a informácie s tým spojené prijímame IBA TELEFONICKY na O2 čísle 0949 388 748 po 14.00 hod každý deň. Ak chcete jazdiť na zľavnený kupón, musíte ho doniesť vytlačený. My netlaćíme a ani neakceptujeme fotky v mobile... Tešíme sa na Vašu návštevu, Motokary Tatry Team';
+
+    $scope.driverTime = 'Čas';
+    $scope.driverName = 'Meno';
+
+    $scope.motoTitle = 'Dráha a motokáry';
+    $scope.motoText1 = 'V našom motokárovom parku máme motokáry s motorom Honda GX 200 (s výkonom 6,5 HP) a Honda GX 270 (s výkonom 9 HP). Aby ste si mohli vychutnať pocit z jazdy na 9 koňovej motokáre, musíte splniť časový limit na 6,5 ke.';
+    $scope.motoText2 = 'Dĺžka dráhy je 230 metrov pri šírke dráhy od 3,5 metra až do 5,5 metra. Na trati je možné dosiahnuť priemernú rýchlosť nad 30 km/h. Striedavé technické úseky s rovinkou Vám zaručene splnia Vaše očakávania.';
+
+    $scope.oneKart = '1 Kára';
+    $scope.threeKart = '3 Káry';
+    $scope.fourKart = '4 Káry';
+    $scope.fiveKart = '5 Kár';
+
+    $scope.maskSell = 'Hygienická kukla predaj 2,00 €';
+    $scope.maskRent = 'Hygienická kukla prenájom 1,00 €';
+
+    $scope.highlightText2 = 'Poskytujeme príjemné posedenie v Bare a organizujeme firemné akcie na objednávku';
+
+    $scope.mapText = 'Motokáry Tatry sa nachádzajú na druhom poschodí bývalej výrobnej haly vo Svite (viď mapu), kde poskytujeme kvalitnú adrenalínovú zábavu na motokárach.';
+
+    $scope.day1 = 'Pondelok';
+    $scope.day2 = 'Utorok';
+    $scope.day3 = 'Streda';
+    $scope.day4 = 'Štvrtok';
+    $scope.day5 = 'Piatok';
+    $scope.day6 = 'Sobota';
+    $scope.day7 = 'Nedeľa';
+
+    $scope.warningTitle = 'Prevádzkové pokyny:';
+
+    $scope.rulesText0 = 'Každý návštevník je povinný pred použitím motokáry zoznámiť sa s týmito prevádzkovými a bezpečnostnými podmienkami. Prevzatím stroja od prevádzkovateľa (obsluhy) k jazde zákazník potvrdzuje, že sa s týmito podmienkami zoznámil a zaväzuje sa k ich dodržiavaniu.';
+    $scope.rulesText1 = 'Nastupovanie do motokár a vystupovanie z motokár sa prevádza výhradne v priestoroch depa za pokynu personálu';
+    $scope.rulesText2 = 'Výjazd z depa a príjazdy do depa riadi personál';
+    $scope.rulesText3 = 'Pri vjazde do depa je povinnosť signalizácie (odbočenie) zdvihnutou rukou pri spomalenej jazde';
+    $scope.rulesText4 = 'V prípade kolízie zákaz vystupovať z motokáry, môže dôjsť k ohrozeniu inou motokárou a následne k úrazu. Zdvihnutím ruky privoláte obslužný personál';
+    $scope.rulesText5 = 'Pri jazde prísny zákaz akéhokoľvek kontaktu s inou motokárou';
+    $scope.rulesText6 = 'Počas jazdy zákaz púšťania volantu a mávania rukami, prípadne nohami';
+    $scope.rulesText7 = 'Prísny zákaz vstupu na dráhu';
+    $scope.rulesText8 = 'Rešpektovať bezpečnostné a obslužné pokyny personálu';
+
+    $scope.rulesText9 = 'Jazda maloletých do 15 rokov len v sprievode rodičov (zákonných zástupcov)';
+    $scope.rulesText10 = 'Pri jazde je povinnosť používať hygienickú kuklu (možnosť zakúpiť pri bare) a ochrannú helmu, ktorá musí byť zapnutá.';
+    $scope.rulesText11 = 'Zákaz jazdenia v nad-rozmernom oblečení ( veľké vetrovky, košele, šále, dlhé vlasy)';
+    $scope.rulesText12 = 'Pozor na horúci výfuk!!! (hlavne pri vystupovaní) - v prípade poškodenia odevu nezodpovedáme za vzniknuté škody';
+    $scope.rulesText13 = 'Zákaz jazdy pri podozrení použitia alkoholu, hypnotizujúcich a omamných látok';
+    $scope.rulesText14 = 'V priestoroch dráhy a depa zákaz fajčenia a vstupu s pohármi';
+    $scope.rulesText15 = 'Doporučujeme vo vlastnom záujme odloženie kľúčov, mobilných telefónov a iných cenností, ktoré môžu vypadnúť z vreciek';
+    $scope.rulesText16 = 'V prípade nedodržania prevádzkových a bezpečnostných pokynov prevádzkovateľ nezodpovedá za škody vzniknuté na zdraví a majetku.';
+
 
     function loadJSON(callback) {
 
         var xobj = new XMLHttpRequest();
         xobj.overrideMimeType("application/json");
-        xobj.open('GET', './svk/top-12-year.json', true); // Replace 'my_data' with the path to your file
+        xobj.open('GET', './svk/top-year.json', true); // Replace 'my_data' with the path to your file
+        xobj.onreadystatechange = function () {
+            if (xobj.readyState == 4 && xobj.status == "200") {
+                // Required use of an anonymous callback as .open will NOT return a value but simply returns undefined in asynchronous mode
+                callback(xobj.responseText);
+            }
+        };
+        xobj.send(null);
+    }
+
+    function loadTopMonth(callback) {
+
+        var xobj = new XMLHttpRequest();
+        xobj.overrideMimeType("application/json");
+        xobj.open('GET', './svk/top-month.json', true); // Replace 'my_data' with the path to your file
         xobj.onreadystatechange = function () {
             if (xobj.readyState == 4 && xobj.status == "200") {
                 // Required use of an anonymous callback as .open will NOT return a value but simply returns undefined in asynchronous mode
@@ -71,7 +139,6 @@ angular.module('myApp.svk', ['ngRoute'])
             var n = month[d.getMonth()];
 
             $scope.actualMonth = n;
-            console.log($scope.actualMonth);
         }
         getDate();
 
@@ -85,17 +152,30 @@ angular.module('myApp.svk', ['ngRoute'])
                 var driverName = '<td>' + value.name + '</td>';
                 var driverTime = '<td>' + value.time + '</td>';
 
-                $('#top-12').append( '<tr>'+
-                        driverId + driverTime + driverName
-                  + '</tr>');
-
                 $('#top-12-year').append( '<tr>'+
                     driverId + driverTime + driverName
                     + '</tr>');
             });
 
-            $('#top-12').find('tr').first().addClass('first-place');
             $('#top-12-year').find('tr').first().addClass('first-place');
+        });
+
+
+        loadTopMonth(function(response) {
+            // Parse JSON string into object
+            var actual_JSON = JSON.parse(response);
+
+            $.each(actual_JSON, function (index, value) {
+                var driverId = '<td>' + value.id + '</td>';
+                var driverName = '<td>' + value.name + '</td>';
+                var driverTime = '<td>' + value.time + '</td>';
+
+                $('#top-12').append( '<tr>'+
+                    driverId + driverTime + driverName
+                    + '</tr>');
+            });
+
+            $('#top-12').find('tr').first().addClass('first-place');
         });
 
         loadPrices(function(response) {
